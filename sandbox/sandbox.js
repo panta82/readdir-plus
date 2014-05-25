47c41e11b729e3258d61d45e96eae90055706303
Ivan Pantic <ivanpantic82@gmail.com>
2014-05-25 11:02:18 +0200

    More features working, taking final shape

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,5 +1,13 @@
 var readdir = require("../lib/readdir-plus");
 
-readdir("../tests/root", function (err, results) {
-	console.log(err, results);
-});
+if (false) {
+	readdir("../tests/root", function (err, results) {
+		console.log(err, results);
+	});
+}
+
+if (true) {
+	readdir("../tests/root", { details: true, recursive: true }, function (err, results) {
+		console.log(err, results);
+	});
+}
