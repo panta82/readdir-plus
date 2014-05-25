1586f2353a29b64b82f9685b7dd378f818022747
Ivan Pantic <ivanpantic82@gmail.com>
2014-05-25 23:47:35 +0200

    Cont.

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,17 +1,30 @@
 var readdir = require("../lib/readdir-plus");
 
+var rootSimple = __dirname + "/root/simple";
+
 module.exports = {
 	setUp: function (callback) {
 		callback();
 	},
 	canWorkWithDefaults: function (test) {
 		test.expect(5);
-		readdir(__dirname + "/root/simple", function (err, results) {
+		readdir(rootSimple, function (err, results) {
 			test.ok(!(err instanceof Error));
 			test.equal(results.length, 2);
 			test.ok(results[0].stat);
 			test.equal(results[0].name, "file1.txt");
 			test.equal(results[1].name, "file2.txt");
+			test.done();
+		});
+	},
+	canReturnJustNames: function (test) {
+		test.expect(4);
+		readdir(rootSimple, { details: false }, function (err, results) {
+			console.log(err);
+			test.ok(!(err instanceof Error));
+			test.equal(results.length, 2);
+			test.equal(results[0], "file1.txt");
+			test.equal(results[1], "file2.txt");
 			test.done();
 		});
 	}
