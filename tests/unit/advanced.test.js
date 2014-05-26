81b8b80b41d8a75b80462f5781e665ecc2635822
Ivan Pantic <ivanpantic82@gmail.com>
2014-05-26 14:36:24 +0200

    Filters refactored, symbolicLinks tested & working

--- tests/unit/advanced.test.js
+++ tests/unit/advanced.test.js
@@ -2,18 +2,26 @@ var libPath = require("path");
 
 var readdir = require("../../lib/readdir-plus");
 
-var rootSimple = libPath.resolve(__dirname + "/../root/simple");
+var rootSimple = libPath.resolve(__dirname + "/../root/simple"),
+	rootAdvanced = libPath.resolve(__dirname + "/../root/advanced");
 
 module.exports = {
-	canGetBasicStat: function (test) {
+	canDetermineFileType: function (test) {
 		test.expect(5);
-		readdir(rootSimple, {stat: true, filters: { directory: true }}, function (err, results) {
+		readdir(rootAdvanced, {stat: true, filter: true}, function (err, results) {
 			test.equal(err, null);
-
-			test.equal(results.length, 3);
-			test.ok(results[0].type, "file");
-			test.ok(results[1].type, "directory");
-			test.ok(results[2].type, "file");
+			test.equal(results.length, 7);
+			results.forEach(function (result) {
+				if (result.name === "file1.txt") {
+					test.ok(result.type, "file");
+				}
+				if (result.name === "sub1") {
+					test.ok(result.type, "directory");
+				}
+				if (result.name === "file1.lnk") {
+					test.ok(result.type, "symbolicLink");
+				}
+			});
 			test.done();
 		});
 	}
