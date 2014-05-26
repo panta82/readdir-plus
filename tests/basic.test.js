8707b04f77514eedda5b0b8d1282de7336b5d2bd
Ivan Pantic <ivanpantic82@gmail.com>
2014-05-26 09:50:24 +0200

    Fixed return types

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,11 +1,10 @@
+var libPath = require("path");
+
 var readdir = require("../lib/readdir-plus");
 
 var rootSimple = __dirname + "/root/simple";
 
 module.exports = {
-	setUp: function (callback) {
-		callback();
-	},
 	canWorkWithDefaults: function (test) {
 		test.expect(5);
 		readdir(rootSimple, function (err, results) {
@@ -17,14 +16,33 @@ module.exports = {
 			test.done();
 		});
 	},
-	canReturnJustNames: function (test) {
+	canReturnNames: function (test) {
 		test.expect(4);
-		readdir(rootSimple, { details: false }, function (err, results) {
-			console.log(err);
+		readdir(rootSimple, { return: readdir.RETURN_TYPES.names }, function (err, results) {
 			test.ok(!(err instanceof Error));
 			test.equal(results.length, 2);
 			test.equal(results[0], "file1.txt");
 			test.equal(results[1], "file2.txt");
+			test.done();
+		});
+	},
+	canReturnRelativePaths: function (test) {
+		test.expect(4);
+		readdir(rootSimple, { return: readdir.RETURN_TYPES.relativePaths }, function (err, results) {
+			test.ok(!(err instanceof Error));
+			test.equal(results.length, 2);
+			test.equal(results[0], "file1.txt");
+			test.equal(results[1], "subdir1/file2.txt");
+			test.done();
+		});
+	},
+	canReturnFullPaths: function (test) {
+		test.expect(4);
+		readdir(rootSimple, { return: readdir.RETURN_TYPES.fullPaths }, function (err, results) {
+			test.ok(!(err instanceof Error));
+			test.equal(results.length, 2);
+			test.equal(results[0], libPath.resolve(rootSimple, "file1.txt"));
+			test.equal(results[1], libPath.resolve(rootSimple, "subdir1/file2.txt"));
 			test.done();
 		});
 	}
