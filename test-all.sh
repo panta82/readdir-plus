6529a792ccef2abf7440254726b84b1bbe41c66f
Ivan Pantic <ivan@pantas.net>
2019-12-22 02:48:03 +0100

    Audit, fix tests

--- test-all.sh
+++ test-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 SCRIPT_PATH=`dirname $0`
-nodeunit `find ${SCRIPT_PATH}/tests/unit -name "*.test.js"`
+$SCRIPT_PATH/node_modules/.bin/nodeunit `find ${SCRIPT_PATH}/tests/unit -name "*.test.js"`
